bdc06a5cbcb4f35e2eb1df78079346e22de4bbb1
vinay lakshmaiah <vinay.lakshmaiah@netfoundry.io>
2025-12-30 12:35:50 +0530

    Add External JWT Signer to terraform provider

--- internal/provider/posture_check_domain_resource.go
+++ internal/provider/posture_check_domain_resource.go
@@ -157,7 +157,7 @@ func (r *postureCheckDomainResource) Create(ctx context.Context, req resource.Cr
 
 	// Convert the payload to JSON
 	jsonData, _ := json.Marshal(payload)
-	fmt.Printf("**********************create resource payload***********************:\n %+v\n", jsonData)
+	fmt.Printf("**********************create resource payload***********************:\n %s\n", jsonData)
 
 	authUrl := fmt.Sprintf("%s/posture-checks", r.resourceConfig.host)
 	cresp, err := CreateZitiResource(authUrl, r.resourceConfig.apiToken, jsonData)
@@ -312,7 +312,7 @@ func (r *postureCheckDomainResource) Update(ctx context.Context, req resource.Up
 
 	// Convert the payload to JSON
 	jsonData, _ := json.Marshal(payload)
-	fmt.Printf("**********************update resource payload***********************:\n %+v\n", jsonData)
+	fmt.Printf("**********************update resource payload***********************:\n %s\n", jsonData)
 
 	authUrl := fmt.Sprintf("%s/posture-checks/%s", r.resourceConfig.host, url.QueryEscape(state.ID.ValueString()))
 	cresp, err := PatchZitiResource(authUrl, r.resourceConfig.apiToken, jsonData)
