ee0dbbb57f70122e92ac309ea3faff20b9a22722
tim <tim@x2a.icu>
2026-02-16 12:38:42 +0100

    Sanitize common package and add retries to requests

--- internal/provider/common.go
+++ internal/provider/common.go
@@ -7,25 +7,26 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/hashicorp/go-retryablehttp"
 	"github.com/rs/zerolog/log"
 )
 
-func CreateZitiResource(authUrl string, sessionToken string, payloadData []byte) (string, error) {
-	transport := &http.Transport{
+func doRequest(method, url, sessionToken string, body []byte) (string, error) {
+	httpClient := retryablehttp.NewClient()
+	httpClient.HTTPClient.Transport = &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-	httpClient := &http.Client{Transport: transport}
-	req, _ := http.NewRequest("POST", authUrl, bytes.NewBuffer(payloadData))
+
+	req, _ := retryablehttp.NewRequest(method, url, bytes.NewBuffer(body))
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("zt-session", sessionToken)
 	resp, err := httpClient.Do(req)
-
 	if err != nil {
-		log.Error().Msgf("Got an error back from the Ziti controller: resourceType: %s, ERR: %v", authUrl, err)
+		log.Error().Msgf("Got an error back from the Ziti controller: resourceType: %s, ERR: %v", url, err)
 		return "", err
 	}
 
-	body, err := io.ReadAll(resp.Body)
+	body, err = io.ReadAll(resp.Body)
 	defer resp.Body.Close()
 	if err != nil {
 		log.Error().Msgf("Error Creating Ziti Resource Response: %v", err)
@@ -42,130 +43,22 @@ func CreateZitiResource(authUrl string, sessionToken string, payloadData []byte)
 	return string(body), nil
 }
 
-func ReadZitiResource(authUrl string, sessionToken string) (string, error) {
-	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	httpClient := &http.Client{Transport: transport}
-	req, _ := http.NewRequest("GET", authUrl, nil)
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("zt-session", sessionToken)
-	resp, err := httpClient.Do(req)
-
-	if err != nil {
-		log.Error().Msgf("Got an error back from the Ziti controller: resourceType: %s, ERR: %v", authUrl, err)
-		return "", err
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		log.Error().Msgf("Error Reading Ziti Resource Response: %v", err)
-		return "", err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		if resp.StatusCode != http.StatusCreated {
-			log.Printf("Unexpected status code: %d %s", resp.StatusCode, resp.Status)
-			return string(body), errors.New(string(body))
-		}
-	}
-
-	return string(body), nil
+func CreateZitiResource(requestURL string, sessionToken string, payloadData []byte) (string, error) {
+	return doRequest(http.MethodPost, requestURL, sessionToken, payloadData)
 }
 
-func UpdateZitiResource(authUrl string, sessionToken string, payloadData []byte) (string, error) {
-	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	httpClient := &http.Client{Transport: transport}
-	req, _ := http.NewRequest("PUT", authUrl, bytes.NewBuffer(payloadData))
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("zt-session", sessionToken)
-	resp, err := httpClient.Do(req)
-
-	if err != nil {
-		log.Error().Msgf("Got an error back from the Ziti controller: resourceType: %s, ERR: %v", authUrl, err)
-		return "", err
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		log.Error().Msgf("Error Updating Ziti Resource Response: %v", err)
-		return "", err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		if resp.StatusCode != http.StatusCreated {
-			log.Printf("Unexpected status code: %d %s", resp.StatusCode, resp.Status)
-			return string(body), errors.New(string(body))
-		}
-	}
-
-	return string(body), nil
+func ReadZitiResource(requestURL string, sessionToken string) (string, error) {
+	return doRequest(http.MethodGet, requestURL, sessionToken, nil)
 }
 
-func PatchZitiResource(authUrl string, sessionToken string, payloadData []byte) (string, error) {
-	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	httpClient := &http.Client{Transport: transport}
-	req, _ := http.NewRequest("PATCH", authUrl, bytes.NewBuffer(payloadData))
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("zt-session", sessionToken)
-	resp, err := httpClient.Do(req)
-
-	if err != nil {
-		log.Error().Msgf("Got an error back from the Ziti controller: resourceType: %s, ERR: %v", authUrl, err)
-		return "", err
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		log.Error().Msgf("Error Updating Ziti Resource Response: %v", err)
-		return "", err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		if resp.StatusCode != http.StatusCreated {
-			log.Printf("Unexpected status code: %d %s", resp.StatusCode, resp.Status)
-			return string(body), errors.New(string(body))
-		}
-	}
-
-	return string(body), nil
+func UpdateZitiResource(requestURL string, sessionToken string, payloadData []byte) (string, error) {
+	return doRequest(http.MethodPut, requestURL, sessionToken, payloadData)
 }
 
-func DeleteZitiResource(authUrl string, sessionToken string) (string, error) {
-	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	httpClient := &http.Client{Transport: transport}
-	req, _ := http.NewRequest("DELETE", authUrl, nil)
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("zt-session", sessionToken)
-	resp, err := httpClient.Do(req)
+func PatchZitiResource(requestURL string, sessionToken string, payloadData []byte) (string, error) {
+	return doRequest(http.MethodPatch, requestURL, sessionToken, payloadData)
+}
 
-	if err != nil {
-		log.Error().Msgf("Got an error back from the Ziti controller: resourceType: %s, ERR: %v", authUrl, err)
-		return "", err
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		log.Error().Msgf("Error Reading Ziti Resource Response: %v", err)
-		return "", err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		if resp.StatusCode != http.StatusCreated {
-			log.Printf("Unexpected status code: %d %s", resp.StatusCode, resp.Status)
-			return string(body), errors.New(string(body))
-		}
-	}
-
-	return string(body), nil
+func DeleteZitiResource(requestURL string, sessionToken string) (string, error) {
+	return doRequest(http.MethodDelete, requestURL, sessionToken, nil)
 }
